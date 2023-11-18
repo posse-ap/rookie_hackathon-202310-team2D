e256d1245218a602e9467d1b60968f064febcc5c
Rintaro <shishi.rin11313@gmail.com>
2023-11-19 02:17:42 +0900

    indivisual page

---
+++ pages/rintaro/script.js
@@ -0,0 +1,45 @@
+$(".openbtn").click(function () {//ボタンがクリックされたら
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+    $(".circle-bg").toggleClass('circleactive');//丸背景にcircleactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスを除去
+    $(".circle-bg").removeClass('circleactive');//丸背景のcircleactiveクラスを除去
+});
+
+
+
+
+var slider;
+var sliderFlag = false;
+var breakpoint = 768;//768px以下の場合
+  
+function sliderSet() {
+        var windowWidth = window.innerWidth;
+        if (windowWidth >= breakpoint && !sliderFlag) {//768px以上は1行でスライダー表示
+            slider = $('.slider').bxSlider({
+            touchEnabled:false,//リンクを有効にするためスライドをマウスでドラッグした際にスライドの切り替えを可能にする機能を無効化
+			mode: 'vertical',//縦スライド指定
+			controls: false,//前後のコントロールを表示させない。
+			auto: 'true',//自動的にスライド
+			pager: false//ページ送り無効化
+		});
+            sliderFlag = true;
+        } else if (windowWidth < breakpoint && sliderFlag) {
+            slider.destroySlider();//bxSliderのOptionであるdestroySliderを使用してスライダーの動きを除去
+            sliderFlag = false;
+        }
+    }
+
+$(window).on('load resize', function() {
+        sliderSet();
+});
+
+//logoの表示
+$(window).on('load',function(){
+    $("#splash").delay(1500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
+    $("#splash_logo").delay(1200).fadeOut('slow');//ロゴを1.2秒（1200ms）待機してからフェードアウト
+  });
